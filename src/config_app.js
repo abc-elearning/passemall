e64c798b24bf7c4cc9f854ad812bdb542b99dfe4
Hiep Nguyen <nxhiep.1996@gmail.com>
2020-12-10 11:50:01 +0700

    asvab 2.0.12

--- src/config_app.js
+++ src/config_app.js
@@ -1,5 +1,5 @@
-export const APP_NEW_DOMAIN = 5722070642065408;
-export const GA_ID = 'UA-167769768-3';
-export const APP_SHORT_NAME = 'cdl';
+export const APP_NEW_DOMAIN = 5734055144325120;
+export const GA_ID = 'UA-167769768-2';
+export const APP_SHORT_NAME = 'asvab';
 export const VERSION = '2.0.12';
-export const GOOGLE_SITE_VERIFICATION = 'alG3R0D0dTXZb4Nn8ZSB8FYspMh_HAlziNimiRYMO24';
+export const GOOGLE_SITE_VERIFICATION = 'kS0dwjBOA9pZaStotM-lbFAW2fP7vk6-usRhQcPogw0';
