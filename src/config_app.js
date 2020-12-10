176bb0fd5fb0812c6faaab22441a53d8c6b1b65d
Hiep Nguyen <nxhiep.1996@gmail.com>
2020-12-10 11:52:26 +0700

    ged 2.0.12

--- src/config_app.js
+++ src/config_app.js
@@ -1,5 +1,5 @@
-export const APP_NEW_DOMAIN = 5722070642065408;
-export const GA_ID = 'UA-167769768-3';
-export const APP_SHORT_NAME = 'cdl';
+export const APP_NEW_DOMAIN = 5296397775536128;
+export const GA_ID = 'UA-167769768-4';
+export const APP_SHORT_NAME = 'ged';
 export const VERSION = '2.0.12';
-export const GOOGLE_SITE_VERIFICATION = 'alG3R0D0dTXZb4Nn8ZSB8FYspMh_HAlziNimiRYMO24';
+export const GOOGLE_SITE_VERIFICATION = 'kb-TFW_3mkPFfxmjCo79K89fbhM9jEz6Og6k2RZ1P8Y';
