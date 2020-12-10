9e256114ab5efa1c087fea0d1b1ff3ed81ae03ab
Hiep Nguyen <nxhiep.1996@gmail.com>
2020-12-10 11:44:24 +0700

    cdl version 2.0.12

--- src/config_app.js
+++ src/config_app.js
@@ -1,5 +1,5 @@
 export const APP_NEW_DOMAIN = 5722070642065408;
 export const GA_ID = 'UA-167769768-3';
 export const APP_SHORT_NAME = 'cdl';
-export const VERSION = '2.0.11';
+export const VERSION = '2.0.12';
 export const GOOGLE_SITE_VERIFICATION = 'alG3R0D0dTXZb4Nn8ZSB8FYspMh_HAlziNimiRYMO24';
