de4a7489273817a7b0e2ecddd5c3011fcef0eb9c
Hiep Nguyen <nxhiep.1996@gmail.com>
2020-12-10 11:53:26 +0700

    teas 2.0.12

--- src/config_app.js
+++ src/config_app.js
@@ -1,5 +1,5 @@
-export const APP_NEW_DOMAIN = 5722070642065408;
-export const GA_ID = 'UA-167769768-3';
-export const APP_SHORT_NAME = 'cdl';
+export const APP_NEW_DOMAIN = 5186025303310336;
+export const GA_ID = 'UA-167769768-5';
+export const APP_SHORT_NAME = 'teas';
 export const VERSION = '2.0.12';
-export const GOOGLE_SITE_VERIFICATION = 'alG3R0D0dTXZb4Nn8ZSB8FYspMh_HAlziNimiRYMO24';
+export const GOOGLE_SITE_VERIFICATION = 'eEkgzIeAy7B-WsySPbD7FAtOnBl7nOqfaDopBHYGkgc';
